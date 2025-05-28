9bb50b946edb38a7b543980fda1d52666f1d8c5c
danielp <dpahima98@gmail.com>
2025-05-28 08:04:56 +0300

    fix unit test

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,5 +14,6 @@ def test_hello(client):
     # בודק שהסטטוס קוד הוא 200 (OK)
     assert response.status_code == 200
     
+    
     # בודק שהתגובה מכילה את הטקסט "Hello, World!"
     assert response.data.decode('utf-8') == "Hello, World!"
